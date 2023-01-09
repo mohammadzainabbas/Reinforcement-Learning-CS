403b133acc6a6a8148d0d6eba78cf52a13cdce82
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-09 21:24:24 +0100

    Minor change

--- src/train_brax.py
+++ src/train_brax.py
@@ -2,7 +2,7 @@ import dm_control
 import brax
 
 # Load the environment.
-env = dm_control.mujoco.wrapper.load('manipulator', 'insert_ball', target_size=0.1)
+env = dm_control.suite.load('manipulator', 'insert_ball', visualize_reward=True)
 
 # Convert the environment to a gym.Env.
 gym_env = brax.dm_control_to_gym_wrapper(env)
