2ef47568a44bbf4f3faf23c7ed0d4f61a211b522
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-09 18:31:38 +0100

    Work in progress

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,9 @@ from dm_control import suite
 import numpy as np
 from typing import List, Tuple
 
+def print_log(text: str, epoch: int, episode: int, reward: float):
+    print(f'Epoch: {epoch}, Episode: {episode}, Reward: {reward} - {text}')
+
 # Define the actor model.
 def actor_model(env: suite.Environment):
     inputs = tf.keras.Input(shape=(env.observation_spec().shape[0],))
