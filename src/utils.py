8c92e18568948717f1924fe4c74cdc2df89c3216
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-09 17:58:23 +0100

    Updated code

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+
+
+# Define the actor model.
+def actor_model():
+    inputs = tf.keras.Input(shape=(env.observation_spec().shape[0],))
+    x = Dense(32, activation='relu')(inputs)
+    x = Dense(32, activation='relu')(x)
+    x = Dense(32, activation='relu')(x)
+    logits = Dense(env.action_spec().shape[0])(x)
+    model = Model(inputs=inputs, outputs=logits)
+    return model
