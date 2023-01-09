4646a7d21ef433ee7f82ecdb83098ec03037ab4b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-09 18:48:42 +0100

    Minor

--- src/train.py
+++ src/train.py
@@ -6,74 +6,73 @@ import tensorflow as tf
 from sac import SACAgent
 from utils import actor_model, critic_model, preprocess_data, print_log
 
-def train
+def train():
+    # Defaults
+    SEED = 42
+    EPOCHS = 1000
+    EPISODES = 10
+    DISCOUNT_FACTOR = 0.99
 
-# Defaults
-SEED = 42
-EPOCHS = 1000
-EPISODES = 10
-DISCOUNT_FACTOR = 0.99
+    # Set the random seed for reproducibility.
+    np.random.seed(SEED)
+    tf.random.set_seed(SEED)
 
-# Set the random seed for reproducibility.
-np.random.seed(SEED)
-tf.random.set_seed(SEED)
+    # Load the `manipulator` environment.
+    env = dm_control.suite.load(
+        'manipulator', 'insert_ball', task_kwargs=dict(target_size='large'))
 
-# Load the `manipulator` environment.
-env = dm_control.suite.load(
-    'manipulator', 'insert_ball', task_kwargs=dict(target_size='large'))
+    # Create the actor and critic models.
+    actor = actor_model(env=env)
+    critic = critic_model(env=env)
 
-# Create the actor and critic models.
-actor = actor_model(env=env)
-critic = critic_model(env=env)
+    # Create the SAC agent.
+    agent = SACAgent(actor, critic)
 
-# Create the SAC agent.
-agent = SACAgent(actor, critic)
+    # Set the discount factor and target entropy.
+    agent.discount_factor = DISCOUNT_FACTOR
+    agent.target_entropy = -np.prod(env.action_spec().shape)
 
-# Set the discount factor and target entropy.
-agent.discount_factor = DISCOUNT_FACTOR
-agent.target_entropy = -np.prod(env.action_spec().shape)
+    # Set the learning rate and optimizer.
+    agent.learning_rate = 1e-3
+    optimizer = tf.keras.optimizers.Adam(learning_rate=agent.learning_rate)
 
-# Set the learning rate and optimizer.
-agent.learning_rate = 1e-3
-optimizer = tf.keras.optimizers.Adam(learning_rate=agent.learning_rate)
+    # Create the checkpoint manager to save and restore the agent.
+    checkpoint_dir = './checkpoints'
+    checkpoint = tf.train.Checkpoint(optimizer=optimizer, agent=agent)
+    manager = tf.train.CheckpointManager(checkpoint, directory=checkpoint_dir, max_to_keep=3)
 
-# Create the checkpoint manager to save and restore the agent.
-checkpoint_dir = './checkpoints'
-checkpoint = tf.train.Checkpoint(optimizer=optimizer, agent=agent)
-manager = tf.train.CheckpointManager(checkpoint, directory=checkpoint_dir, max_to_keep=3)
+    # Restore the checkpoint if it exists.
+    checkpoint.restore(manager.latest_checkpoint)
+    if manager.latest_checkpoint:
+        print_log(f'Restored from {manager.latest_checkpoint}')
+    else:
+        print_log('Initializing from scratch.')
 
-# Restore the checkpoint if it exists.
-checkpoint.restore(manager.latest_checkpoint)
-if manager.latest_checkpoint:
-    print_log(f'Restored from {manager.latest_checkpoint}')
-else:
-    print_log('Initializing from scratch.')
+    # Training loop.
+    for epoch in range(EPOCHS):
+        # Collect a few episodes of data.
+        episodes = []
+        for _ in range(EPISODES):
+            episode = []
+            time_step = env.reset()
+            while not time_step.last():
+                action = agent.select_action(time_step)
+                next_time_step = env.step(action)
+                episode.append((time_step, action, next_time_step))
+                time_step = next_time_step
+            episodes.append(episode)
 
-# Training loop.
-for epoch in range(EPOCHS):
-    # Collect a few episodes of data.
-    episodes = []
-    for _ in range(EPISODES):
-        episode = []
-        time_step = env.reset()
-        while not time_step.last():
-            action = agent.select_action(time_step)
-            next_time_step = env.step(action)
-            episode.append((time_step, action, next_time_step))
-            time_step = next_time_step
-        episodes.append(episode)
+        # Preprocess the data.
+        observations, actions, rewards, next_observations, dones = preprocess_data(episodes)
 
-    # Preprocess the data.
-    observations, actions, rewards, next_observations, dones = preprocess_data(episodes)
+        # Update the agent.
+        agent.train_step(observations, actions, rewards, next_observations, dones, optimizer)
 
-    # Update the agent.
-    agent.train_step(observations, actions, rewards, next_observations, dones, optimizer)
+        # Save the checkpoint.
+        manager.save()
 
-    # Save the checkpoint.
-    manager.save()
-
-# Close the environment.
-env.close()
+    # Close the environment.
+    env.close()
 
 if __name__ == "__main__":
     pass
