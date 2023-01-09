d5717dcc0b0bf28d802fe82f9690bb27e582721d
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-09 21:19:03 +0100

    Minor changes

--- src/train_brax.py
+++ src/train_brax.py
@@ -7,3 +7,18 @@ env = dm_control.mujoco.wrapper.load('manipulator', 'insert_ball', target_size=0
 # Convert the environment to a gym.Env.
 gym_env = brax.dm_control_to_gym_wrapper(env)
 
+# Train the agent.
+agent = brax.sac(gym_env,
+                 replay_buffer_size=10000,
+                 num_episodes=1000,
+                 num_steps_per_epoch=1000,
+                 num_steps_per_eval=1000,
+                 epochs_per_eval=5,
+                 max_steps=5000000)
+
+# Test the agent.
+average_reward, std_dev = brax.evaluate(gym_env, agent, num_episodes=100)
+print(f'Average reward: {average_reward:.2f} +/- {std_dev:.2f}')
+
+# Animate the agent.
+brax.animate(gym_env, agent)
