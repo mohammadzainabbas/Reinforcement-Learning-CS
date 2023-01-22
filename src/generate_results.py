56f107960b29f01305917beb1b4c465ca6bdb4df
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-22 01:10:57 +0100

    Work in progress

--- src/generate_results.py
+++ src/generate_results.py
@@ -1,6 +1,7 @@
 from json import loads
 from typing import Union, Dict, Any
 from pathlib import Path
+from os.path import join
 
 def read_json(path: Union[str, Path]) -> Dict[str, Any]:
 	with open(path, "r") as f:
